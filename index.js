0f11af0d6247ab910ff46e17f6556be09ad95eca
Jake Wehder <jw@email.comjake>
2022-07-23 12:31:03 -0500

    commit tree db7b52b6e905782c51c8ae9da6e37480aadbfb3e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = arr => arr.slice(0,2);
+
+console.log(returnFirstTwoDrivers(drivers))
+
+const returnLastTwoDrivers = arr => arr.slice(2);
+
+console.log(returnLastTwoDrivers(drivers))
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(int) {
+    return fare => fare * int;
+}
+
+function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+function fareTripler(fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    if (func === selectingDrivers[0]) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    }
+    else if (func === selectingDrivers[1]) {
+        return returnLastTwoDrivers(arrayOfDrivers);
+    }
+}
+
+console.log(createFareMultiplier(2)(2));
